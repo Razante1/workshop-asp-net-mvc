b8c1f5a5674b7c62576dd673c4a0d72abfac95f5
Antonio Guilherme Macedo Camara <antonioguilhermemc@gmail.com>
2023-05-08 14:11:31 -0300

    teste

---
+++ SalesNewApp/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace SalesNewApp.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
