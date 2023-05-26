63c405f95d278fb7867b5c4210233f9aeee289b4
Antonio Guilherme Macedo Camara <antonioguilhermemc@gmail.com>
2023-05-26 17:37:14 -0300

    Error alerts and Edit button implementation

---
+++ SalesNewApp/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesNewApp.Services.Exceptions
+{
+    public class NotFoundException: ApplicationException
+    {
+        public NotFoundException(string message): base(message)
+        {
+
+        }
+    }
+}
--- SalesNewApp/Services/SellerService.cs
+++ SalesNewApp/Services/SellerService.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesNewApp.Models;
-
+using SalesNewApp.Services.Exceptions;
 
 namespace SalesNewApp.Services
 {
@@ -35,5 +35,23 @@ namespace SalesNewApp.Services
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         } 
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
     }
 }
